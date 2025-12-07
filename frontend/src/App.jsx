fa0028a3644b97781890b1f29292e74424e4992e
YourGitHubUsername <YourEmail@example.com>
2025-12-07 22:27:42 +0500

    Add Navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+import Navbar from './components/shared/Navbar';
+
+function App() {
+  console.log('App rendering')
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
