7d25a6717988a3fdd0e5e5da1c2016dbef701dc6
YourGitHubUsername <YourEmail@example.com>
2025-12-09 13:20:20 +0500

    Add login/Signup

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,11 +1,12 @@
-import { Link, LogIn, User2 } from 'lucide-react'
+import { LogIn, User2 } from 'lucide-react'
 import React from 'react'
-import { Links, NavLink } from 'react-router-dom'
+import { Link, Links, NavLink } from 'react-router-dom'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className='bg-white'>
       <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
@@ -18,35 +19,44 @@ const Navbar = () => {
             <li>Jobs</li>
             <li>Browse</li>
           </ul>
+          {
+            !user ? (
+              <div className='flex items-center gap-2'>
+                <Link to="/login"><Button variant="outline">LogIn</Button></Link>
+                <Link to="/signup"><Button className="bg-[#6A38C2] hover:bg-[#5b2fa7]">SignUp</Button></Link>
+              </div>
+            ) : (
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                  </Avatar>
+                </PopoverTrigger>
+                <PopoverContent className='w-80'>
+                  <div className='flex gap-4 space-y-2'>
+                    <Avatar className="cursor-pointer">
+                      <AvatarImage src="https://github.com/shadcn.png" />
+                    </Avatar>
+                    <div>
+                      <h4 className='font-medium'>Affan Mern Stack</h4>
+                      <p className='text-sm text-muted-foreground'>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                  </div>
+                  <div className='flex flex-col  text-gray-600 w-fit items-start my-2'>
+                    <div className='flex items-center'>
+                      <User2 />
+                      <Button className="cursor-pointer" variant="link">View Profile</Button>
+                    </div>
+                    <div className='flex items-center'>
+                      <LogIn />
+                      <Button className="cursor-pointer" variant="link">Logout</Button>
+                    </div>
+                  </div>
+                </PopoverContent>
+              </Popover>
+            )
+          }
 
-          <Popover>
-            <PopoverTrigger asChild>
-              <Avatar className="cursor-pointer">
-                <AvatarImage src="https://github.com/shadcn.png" />
-              </Avatar>
-            </PopoverTrigger>
-            <PopoverContent className='w-80'>
-              <div className='flex gap-4 space-y-2'>
-                <Avatar className="cursor-pointer">
-                  <AvatarImage src="https://github.com/shadcn.png" />
-                </Avatar>
-                <div>
-                  <h4 className='font-medium'>Affan Mern Stack</h4>
-                  <p className='text-sm text-muted-foreground'>Lorem ipsum dolor sit amet.</p>
-                </div>
-              </div>
-              <div className='flex flex-col  text-gray-600 w-fit items-start my-2'>
-                <div className='flex items-center'>
-                  <User2 />
-                  <Button className="cursor-pointer" variant="link">View Profile</Button>
-                </div>
-                <div className='flex items-center'>
-                  <LogIn/>
-                  <Button className="cursor-pointer" variant="link">Logout</Button>
-                </div>
-              </div>
-            </PopoverContent>
-          </Popover>
         </div>
       </div>
     </div>
