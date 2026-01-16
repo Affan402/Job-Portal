61e3dbda4103b7b682d5af0379ab186c41cf5073
Muhammad Affan Shahnawaz <affan.snz@gmail.com>
2026-01-17 00:30:22 +0500

    feat: enhance company registration with validation and add company retrieval methods

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,3 +1,5 @@
+import { Company } from "../models/company.model.js";
+
 
 export const companyRegister = async(req, res) =>{
     try {
@@ -5,6 +7,40 @@ export const companyRegister = async(req, res) =>{
         if(!companyName){
             return res.status(400).json({message: "Company name is required", success: false});
         }
+        let company = await Company.findOne({name: companyName});
+        if (company) {
+            return res.status(409).json({message: "Company already exists", success: false});
+        }
+        company = await Company.create({
+            name: companyName,
+            userID: req.id,
+        })
+        
+        return res.status(201).json({message: "Company registered successfully", company, success: true});
+    } catch (error) {
+        console.log(error);
+    }
+}
+export const getCompany = async(req, res) => {
+    try {
+        const userId = req.id; //logged in user id from middleware
+        const companies = await Company.find({userId});
+        if(!companies) {
+            return res.status(404).json({message: "No companies found", success: false});
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+export const getCompanyById = async(req, res) => {
+    try {
+        const companyId = req.params.id;
+        const company = await Company.findById(companyId);
+        if(!company) {
+            return res.status(404).json({message: "Company not found", success: false});
+        }
+        return res.status(200).json({company, success: true});
     } catch (error) {
         console.log(error);
     }
